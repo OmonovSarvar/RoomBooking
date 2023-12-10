584a06c4a5bbd032cd09165c345b6b372e54f95d
OmonovSarvar <sarvaromon33@gmail.com>
2023-12-10 21:55:45 +0500

    Added Models for Room Booking

--- room_api/models.py
+++ room_api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Room(models.Model):
+    name = models.CharField(max_length=100)
+    room_number = models.SmallIntegerField()
+    booked = models.BooleanField(default=False)
+    start = models.DateTimeField(null=True, blank=True)
+    end = models.DateTimeField(null=True, blank=True)
+
+    def str(self):
+        return self.name
